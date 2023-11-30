4691ea0fdfac94f6d796b01682439015da2c15d7
CesarLeiva26 <cesaremanuelleivajuro@gmail.com>
2023-11-30 16:07:14 -0500

    Fastrack_DSWll_Grupo7

---
+++ src/main/java/pe/edu/cibertec/Fastrack_DSWll_Grupo7/FastrackDsWllGrupo7Application.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.Fastrack_DSWll_Grupo7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastrackDsWllGrupo7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastrackDsWllGrupo7Application.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/Fastrack_DSWll_Grupo7/FastrackDsWllGrupo7ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.Fastrack_DSWll_Grupo7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastrackDsWllGrupo7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
